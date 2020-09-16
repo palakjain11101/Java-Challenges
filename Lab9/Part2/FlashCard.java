3f0326bddddce4b32b1b35d4c73a0f94db237ab8
Palak Jain <palakjain11101@gmail.com>
2020-09-17 01:45:26 +0200

    Create FlashCard.java

---
+++ Lab9/Part2/FlashCard.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class FlashCard{
+	String question;
+	String answer;
+	//instance variable that will hold the ArrayList of FlashCards
+
+	public FlashCard(String question, String answer){
+		this.question = question;
+		this.answer = answer;
+	}
+
+	public String getQuestion(){
+		return question;
+	}
+
+	public String getAnswer(){
+		return answer;
+	}
+
+		
+}
+
+	
