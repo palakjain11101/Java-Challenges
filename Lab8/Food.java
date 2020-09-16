511a01d1c36249b33918accc8be71666d96afcb6
Palak Jain <palakjain11101@gmail.com>
2020-09-17 01:34:32 +0200

    Add files via upload

---
+++ Lab8/Food.java
@@ -0,0 +1,16 @@
+//This is the superclass for Meat and Plant. 
+public class Food{
+	public String name = "food";
+	
+		//Sets the name of the food
+		public Food(String name){
+			String newName = name;
+		}
+	
+		//Returns te name of the food
+		public String getName(){
+			return name;
+		}
+
+}
+	
