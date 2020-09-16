1d47632240a95ee5af573bad870b08a39523e10b
Palak Jain <palakjain11101@gmail.com>
2020-09-17 01:37:07 +0200

    Delete Meat.java

--- Lab8/Meat.java
+++
@@ -1,9 +0,0 @@
-//This class is a subclass of the Food class, and therefore extends it to inherit its properties.
-public class Meat extends Food{
-
-	//The super class allows constructor Meat to access the getName method in the Food class.
-	public Meat(String name){
-	super(name);
-	}
-	
-}
