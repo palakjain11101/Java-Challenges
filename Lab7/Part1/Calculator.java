27a3bf15e084ad1dc2eb92c339cc97225afd3489
Palak Jain <palakjain11101@gmail.com>
2020-09-17 01:29:10 +0200

    Create Calculator.java

---
+++ Lab7/Part1/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+	Double x;
+	/*
+	* Chops up input on ' ' then decides whether to add or multiply.
+	* If the string does not contain a valid format returns null.
+	*/
+	public Double x(String x){
+		Double returnValue = 0.0;
+		return returnValue;
+	}
+	
+	/*
+	* Adds the parameter x to the instance variable x and returns the answer as a Double.
+	*/
+	public Double x(Double x){
+		System.out.println("== Adding ==");
+		Double returnValue = 0.0;
+		return returnValue;
+	}
+	
+	/*
+	* Multiplies the parameter x by instance variable x and return the value as a Double.
+	*/
+	public Double x(double x){
+		System.out.println("== Multiplying ==");
+		Double returnValue = 0.0;
+		return returnValue;
+	}	
+}
