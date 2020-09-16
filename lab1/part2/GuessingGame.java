b47df62aff11ed9a29340c0a17f82e7dcf704ead
Palak Jain <palakjain11101@gmail.com>
2020-09-17 00:07:52 +0200

    Create GuessingGame.java

---
+++ lab1/part2/GuessingGame.java
@@ -0,0 +1,26 @@
+public class GuessingGame{
+ 
+	public static void main(String[] args){
+		Integer numberToGuess;
+		Integer guessedNumber;
+ 
+		Toolbox myToolbox = new Toolbox();
+ 
+		System.out.println("Welcome to Guessing Game");
+		numberToGuess = myToolbox.getRandomInteger(10);
+		guessedNumber = myToolbox.readIntegerFromCmd();
+
+		if(numberToGuess < guessedNumber){
+			System.out.println("The number is too high");
+		}
+
+		else if(numberToGuess > guessedNumber){
+			System.out.println("The number is too low");
+		}
+
+		else{
+			System.out.println("Your guess was right");
+		}
+	
+	}
+}
