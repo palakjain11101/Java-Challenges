315b4a777593833741649fec0fd67f1f2039a2b2
Palak Jain <palakjain11101@gmail.com>
2020-09-17 00:52:29 +0200

    Create Main.java

---
+++ Lab4/Part3/Main.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.*;
+
+public class Main{
+	
+	public static void main(String[] args){
+		UserGroup myUserGroup = new UserGroup();
+		UserGroup administrators = new UserGroup();
+
+		myUserGroup.addSampleData();
+		myUserGroup.printUsernames();
+
+		Iterator<User> it = myUserGroup.getUserIterator();
+		User u;
+		while(it.hasNext()){
+			u = it.next();
+			if(u.getUserType().equals("admin")){ 		
+				administrators.getUsers().add(u);
+			}
+			
+		}
+
+		administrators.printUsernames();
+	    int index = (administrators.getUsers().size() - 1);
+		administrators.getUsers().get(index).setUserType("User");
+		
+
+		myUserGroup.printUsernames();
+		administrators.printUsernames();
+	
+		// this is an administrators array not a user array. so this can cause confusion 
+	}
+}
